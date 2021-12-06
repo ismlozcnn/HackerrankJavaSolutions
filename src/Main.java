b98353ebd81b09911fe50f7addd6aac78a9cb068
İsmail Özcan <l5215067@std.yildiz.edu.tr>
2021-12-06 23:40:54 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //Java Stdin and Stdout II
+        /*Scanner scan=new Scanner(System.in);
+        System.out.println("Bir int değer giriniz: " );
+        int i=scan.nextInt();
+        System.out.println("Bir double değer giriniz: " );
+        double d=scan.nextDouble();
+        System.out.println("Bir string değer giriniz: " );
+
+        String s=scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+*/
+
+
+
+    }
+}
